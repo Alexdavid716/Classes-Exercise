1825fde28fecb7ec3ad303ab58a68d853355ced3
Alex David <alexdavid716@gmail.com>
2024-02-27 10:51:28 -0500

    Classes Repo

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Classes
+﻿using System;
+
+// Define the public Car class
+public class Car
 {
-    public class Program
+    // Public properties
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // Create a new instance of the Car class
+        Car myCar = new Car();
+
+        // Set values in the properties for the object
+        myCar.Make = "Toyota";
+        myCar.Model = "Camry";
+        myCar.Year = 2020;
+
+        // Print the values of each property to the Console
+        Console.WriteLine($"Make: {myCar.Make}");
+        Console.WriteLine($"Model: {myCar.Model}");
+        Console.WriteLine($"Year: {myCar.Year}");
     }
 }
